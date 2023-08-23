89b88f5e146f16856bb4d6a82c9a48a678c85740
Ddraigan <lkjjones1999@gmail.com>
2023-08-23 18:45:11 +0100

    treesitter changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,6 +10,9 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
+	ensure_installed = { "lua", "rust", "javascript", "typescript" },
+	sync_install = false,
+	auto_install = true,
             highlight = {
                 enable = true,
             },
