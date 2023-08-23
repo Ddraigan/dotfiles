de12edfad12f67164225e0c566a2d90f1e3583ac
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-08-23 17:24:41 +0100

    colour edit

--- lua/plugins/colours.lua
+++ lua/plugins/colours.lua
@@ -9,8 +9,6 @@ return {
 			show_end_of_buffer = true,    -- default false
 			-- use transparent background
 			transparent_bg = true,        -- default false
-			-- set custom lualine background color
-			lualine_bg_color = "#44475a", -- default nil
 			-- set italic comment
 			italic_comment = true,        -- default false
 			-- overrides the default highlights with table see `:h synIDattr`
