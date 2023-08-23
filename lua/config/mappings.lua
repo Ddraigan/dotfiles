79dfb30b8bd2ef67f94fe45224d8ec0d7f405b51
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-08-23 15:41:21 +0100

    file hierachy changes

---
+++ lua/config/mappings.lua
@@ -0,0 +1,2 @@
+vim.g.mapleader = " " -- Set <space> as leader key
+vim.keymap.set("n", "<leader>-", vim.cmd.Ex)
---
+++ lua/config/plugins.lua
@@ -0,0 +1,52 @@
+  -- Navigating (Telescope/Tree/Refactor)
+  {
+    "nvim-pack/nvim-spectre",
+    lazy = true,
+    keys = {
+      {
+        "<Leader>pr",
+        "<cmd>lua require('spectre').open_visual({select_word=true})<CR>",
+        desc = "refactor",
+      },
+      {
+        "<Leader>pr",
+        "<cmd>lua require('spectre').open_visual()<CR>",
+        mode = "v",
+        desc = "refactor",
+      }
+    }
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    lazy = false,
+    config = function()
+      require("plugins.telescope")
+    end,
+    dependencies = {
+      { "nvim-lua/popup.nvim" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      { "cljoly/telescope-repo.nvim" },
+    },
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = {
+      "NvimTreeOpen",
+      "NvimTreeClose",
+      "NvimTreeToggle",
+      "NvimTreeFindFile",
+      "NvimTreeFindFileToggle",
+    },
+    keys = {
+      { "<C-e>", "<cmd>lua require('nvim-tree.api').tree.toggle()<CR>", desc = "NvimTree" },
+    },
+    config = function()
+      require("plugins.tree")
+    end,
+  },
+  {
+    "gbprod/stay-in-place.nvim",
+    lazy = false,
+    config = true, -- run require("stay-in-place").setup()
+  },
