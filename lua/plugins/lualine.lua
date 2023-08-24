43b853172d78f1f3be36545ac3bad1ee28f4246a
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-08-24 16:59:19 +0100

    Edit lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,10 +6,10 @@ return {
 	config = function ()
 		require('lualine').setup {
 			options = {
-				icons_enabled = true,
+				icons_enabled = false,
 				theme = 'auto',
-				component_separators = { left = '', right = ''},
-				section_separators = { left = '', right = ''},
+				component_separators = '|',
+				section_separators = '',
 				disabled_filetypes = {
 					statusline = {},
 					winbar = {},
