c87d9f58fe4a0257d6cf2e33e90f6392c498b259
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-08-24 12:17:50 +0100

    Added some tmux keybinds

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -9,6 +9,15 @@ local function bind(op, outer_opts)
     end
 end
 
+M.general = {
+	n = {
+		["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+		["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+		["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+		["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+	}
+}
+
 M.nmap = bind("n", { noremap = false })
 M.nnoremap = bind("n")
 M.vnoremap = bind("v")
