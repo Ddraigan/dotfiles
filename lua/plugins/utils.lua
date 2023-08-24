032fef708d819126985ea9393097af1646fbebaa
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-08-24 16:35:03 +0100

    Added whichkey plugin

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -22,4 +22,14 @@ return {
 		config = true,
 	},
 	"Saecki/crates.nvim",
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+		}
+	}
 }
