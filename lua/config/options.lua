28ed32ac37a546da8ac55b7a1c319d8627ea4e33
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-08-24 09:28:57 +0100

    Commented out some file opts

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,10 +7,10 @@ vim.opt.shiftwidth = 4
 
 vim.opt.smartindent = true
 
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undofile = true
+-- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- vim.opt.swapfile = false
+-- vim.opt.backup = false
+-- vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
