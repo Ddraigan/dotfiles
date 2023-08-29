6bb58db245917fe4de82bb2756a6d6d6654e32cd
Ddraigan <lkjjones1999@gmail.com>
2023-08-30 00:10:17 +0100

    got mappings working

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -8,3 +8,13 @@ vim.keymap.set("n", "<leader>xd", function() require("trouble").open("document_d
 vim.keymap.set("n", "<leader>xq", function() require("trouble").open("quickfix") end)
 vim.keymap.set("n", "<leader>xl", function() require("trouble").open("loclist") end)
 vim.keymap.set("n", "gR", function() require("trouble").open("lsp_references") end)
+
+local mappings = require("keymap").general
+for mode, values in pairs(mappings) do
+    for keybind, mapping_info in pairs(values) do
+        local opts = {
+            desc = mapping_info[2]
+        }
+        vim.keymap.set(mode, keybind, mapping_info[1], opts)
+    end
+end
