d305430f350921df1adc7edc2ede54a229fcc1fb
Ddraigan <lkjjones1999@gmail.com>
2023-08-29 19:32:12 +0100

    Some changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,30 +1,30 @@
---return {
---    "nvim-treesitter/nvim-treesitter",
---    dependencies = {
---        {
---            "nvim-treesitter/nvim-treesitter-context",
---            name = "treesitter-context",
---            config = true,
---        },
---    },
---    build = ":TSUpdate",
---    config = function()
---        require("nvim-treesitter.configs").setup({
---	ensure_installed = { "lua", "rust", "javascript", "typescript", "tsx", "vim", "vimdoc" },
---	sync_install = false,
---	auto_install = true,
---            highlight = {
---                enable = true,
---            },
---            incremental_selection = {
---                enable = true,
---                keymaps = {
---                    init_selection = "<CR>",
---                    node_incremental = "<CR>",
---                    scope_incremental = "<S-CR>",
---                    node_decremental = "<BS>",
---                },
---            },
---        })
---    end,
---}
+return {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+	{
+	    "nvim-treesitter/nvim-treesitter-context",
+	    name = "treesitter-context",
+	    config = true,
+	},
+    },
+    build = ":TSUpdate",
+    config = function()
+	require("nvim-treesitter.configs").setup({
+	    ensure_installed = { "lua", "rust", "javascript", "typescript", "tsx", "vim", "vimdoc" },
+	    sync_install = false,
+	    auto_install = true,
+	    highlight = {
+		enable = true,
+	    },
+	    incremental_selection = {
+		enable = true,
+		keymaps = {
+		    init_selection = "<CR>",
+		    node_incremental = "<CR>",
+		    scope_incremental = "<S-CR>",
+		    node_decremental = "<BS>",
+		},
+	    },
+	})
+    end,
+}
