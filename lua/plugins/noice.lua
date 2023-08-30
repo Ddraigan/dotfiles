2cec296238d587b58882c3cf0b895a4b15a12175
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-08-30 16:56:13 +0100

    noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -11,5 +11,44 @@ return {
 	--   `nvim-notify` is only needed, if you want to use the notification view.
 	--   If not available, we use `mini` as the fallback
 	"rcarriga/nvim-notify",
-    }
+    },
+    commands = {
+	history = {
+	    -- options for the message history that you get with `:Noice`
+	    view = "split",
+	    opts = { enter = true, format = "details" },
+	    filter = {
+		any = {
+		    { event = "notify" },
+		    { error = true },
+		    { warning = true },
+		    { event = "msg_show", kind = { "" } },
+		    { event = "lsp", kind = "message" },
+		},
+	    },
+	},
+	-- :Noice last
+	last = {
+	    view = "popup",
+	    opts = { enter = true, format = "details" },
+	    filter = {
+		any = {
+		    { event = "notify" },
+		    { error = true },
+		    { warning = true },
+		    { event = "msg_show", kind = { "" } },
+		    { event = "lsp", kind = "message" },
+		},
+	    },
+	    filter_opts = { count = 1 },
+	},
+	-- :Noice errors
+	errors = {
+	    -- options for the message history that you get with `:Noice`
+	    view = "popup",
+	    opts = { enter = true, format = "details" },
+	    filter = { error = true },
+	    filter_opts = { reverse = true },
+	},
+    },
 }
