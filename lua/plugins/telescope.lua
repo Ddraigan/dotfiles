06dd8d7ec373b00015bef95e37abcd32909dfc1c
Ddraigan <lkjjones1999@gmail.com>
2023-08-31 22:29:25 +0100

    telescope extensions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,53 +5,58 @@ return {
 		"nvim-lua/plenary.nvim",
 		"sharkdp/fd",
 		"wesleimp/telescope-windowizer.nvim",
-	--	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", config = function() require("telescope").load_extension("fzf") end, },
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 	cmd = "Telescope",
-	opts = {
-		defaults = {
-			mappings = {
-				i = {
-					["<c-t>"] = function(...)
-						return require("trouble.providers.telescope").open_with_trouble(...)
-					end,
-					["<a-t>"] = function(...)
-						return require("trouble.providers.telescope").open_selected_with_trouble(...)
-					end,
+	config = function ()
+		local opts = {
+			defaults = {
+				mappings = {
+					i = {
+						["<c-t>"] = function(...)
+							return require("trouble.providers.telescope").open_with_trouble(...)
+						end,
+						["<a-t>"] = function(...)
+							return require("trouble.providers.telescope").open_selected_with_trouble(...)
+						end,
+					},
+					n = {
+						["<c-t>"] = function(...)
+							return require("trouble.providers.telescope").open_with_trouble(...)
+						end,
+						["<a-t>"] = function(...)
+							return require("trouble.providers.telescope").open_selected_with_trouble(...)
+						end,
+					}
 				},
-				n = {
-					["<c-t>"] = function(...)
-						return require("trouble.providers.telescope").open_with_trouble(...)
-					end,
-					["<a-t>"] = function(...)
-						return require("trouble.providers.telescope").open_selected_with_trouble(...)
-					end,
-				}
-			},
-			vimgrep_arguments = {
-				"rg",
-				"--color=never",
-				"--no-heading",
-				"--with-filename",
-				"--line-number",
-				"--column",
-				"--smart-case",
-			},
-			layout_config = {
-				horizontal = {
-					preview_width = 0.5,
-					results_width = 0.8,
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
 				},
-				width = 0.87,
-				height = 0.80,
-				preview_cutoff = 50,
+				layout_config = {
+					horizontal = {
+						preview_width = 0.5,
+						results_width = 0.8,
+					},
+					width = 0.87,
+					height = 0.80,
+					preview_cutoff = 50,
+				},
+				file_ignore_patterns = { "node_modules", ".git" },
 			},
-			file_ignore_patterns = { "node_modules", ".git" },
-		},
-		extensions = {
-			windowizer = {
-				find_cmd = "fd" -- find command. Available options [ find | fd | rg ] (defaults to "fd")
+			extensions = {
+				windowizer = {
+					find_cmd = "fd" -- find command. Available options [ find | fd | rg ] (defaults to "fd")
+				},
 			},
-		},
-	},
+		}
+		require("telescope").setup(opts)
+		require("telescope").load_extension("windowizer")
+		require("telescope").load_extension("fzf")
+	end,
 }
