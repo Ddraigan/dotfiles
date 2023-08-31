753768fc46828bbe5aceabe1c38eb7887a2a8678
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-08-31 17:08:37 +0100

    O

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,17 +1,20 @@
 return {
-    "rcarriga/nvim-notify",
-    keys = {
-	{
-	    "<leader>un", function() require("notify").dismiss({ silent = true, pending = true }) end, desc = "Dismiss all Notifications",
+	"rcarriga/nvim-notify",
+	event = "BufReadPre",
+	keys = {
+		{
+			"<leader>un", function() require("notify").dismiss({ silent = true, pending = true }) end, desc = "Dismiss all Notifications",
+		},
 	},
-    },
-    opts = {
-	timeout = 10000,
-	max_height = function()
-	    return math.floor(vim.o.lines * 0.75)
-	end,
-	max_width = function()
-	    return math.floor(vim.o.columns * 0.75)
-	end,
-    },
+	opts = {
+		timeout = 10000,
+		stages = "fade_in_slide_out",
+		fps = 120,
+		max_height = function()
+			return math.floor(vim.o.lines * 0.75)
+		end,
+		max_width = function()
+			return math.floor(vim.o.columns * 0.75)
+		end,
+	},
 }
