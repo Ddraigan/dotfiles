971252add6776dce89caee00e301e0347ac4ab4d
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-09-20 09:52:16 +0100

    Working lsp

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,36 +1,69 @@
-local M = {
+return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		{ "williamboman/mason.nvim", config = true },
 		{ "hrsh7th/nvim-cmp" },
 		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "litao91/lsp_lines" },
 	},
-}
+	config = function()
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+		capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-M.config = function()
-	local lspconfig = require("lspconfig")
-	local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-	capabilities.textDocument.completion.completionItem.snippetSupport = true
+		local on_attach = function()
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { noremap = true, desc = "Go To Declaration", silent = true, buffer = 0 })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, desc = "Go To Definition", silent = true, buffer = 0 })
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, desc = "Hover", silent = true, buffer = 0 })
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { noremap = true, desc = "Go To Implementation", silent = true, buffer = 0 })
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, { noremap = true, desc = "Rename", silent = true, buffer = 0 })
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, { noremap = true, desc = "Go To References", silent = true, buffer = 0 })
+			vim.keymap.set("n", "<space>f", function()
+				vim.lsp.buf.format({ async = true })
+			end, { noremap = true, desc = "Format", silent = true, buffer = 0 })
+			vim.keymap.set("n", "<space>df", vim.diagnostic.goto_next, { noremap = true, desc = "Go To Next Diagnostic", silent = true, buffer = 0 })
+		end
 
-	local on_attach = function()
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { noremap = true, desc = "Go To Declaration", silent = true, buffer = 0 })
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, desc = "Go To Definition", silent = true, buffer = 0 })
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, desc = "Hover", silent = true, buffer = 0 })
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { noremap = true, desc = "Go To Implementation", silent = true, buffer = 0 })
-		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, { noremap = true, desc = "Rename", silent = true, buffer = 0 })
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, { noremap = true, desc = "Go To References", silent = true, buffer = 0 })
-		vim.keymap.set("n", "<space>f", function()
-			vim.lsp.buf.format({ async = true })
-		end, { noremap = true, desc = "Format", silent = true, buffer = 0 })
-		vim.keymap.set("n", "<space>df", vim.diagnostic.goto_next, { noremap = true, desc = "Go To Next Diagnostic", silent = true, buffer = 0 })
-	end
+		local servers = { "lua_ls", "pyright", "html", "tsserver", "rust_analyzer" }
+		for _, lsp in pairs(servers) do
+			lspconfig[lsp].setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+		end
 
-	local servers = { "lua_ls", "pyright", "html", "tsserver", "rust_analyzer" }
-	for _, lsp in pairs(servers) do
-		lspconfig[lsp].setup({
+		-- tsserver
+		lspconfig.tsserver.setup({
 			capabilities = capabilities,
-			on_attach = on_attach,
+		})
+		-- rust_analyzer
+		lspconfig.rust_analyzer.setup({
+			capabilities = capabilities,
+			-- Server-specific settings. See `:help lspconfig-setup`
+			settings = {
+				["rust-analyzer"] = {},
+			},
+		})
+		-- html
+		lspconfig.html.setup({
+			capabilities = capabilities,
+		})
+		-- configure emmet language server
+		lspconfig.emmet_ls.setup({
+			capabilities = capabilities,
+			filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+		})
+		-- Lua LS
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
 			settings = {
 				Lua = {
 					diagnostics = {
@@ -39,12 +72,15 @@ M.config = function()
 				},
 			},
 		})
+		-- CSS LS
+		lspconfig.cssls.setup({
+			capabilities = capabilities,
+		})
+		-- Tailwind
+		-- Support for tailwind auto completion
+		-- install the tailwind server : "sudo npm install -g @tailwindcss/language-server"
+		lspconfig.tailwindcss.setup({
+			capabilities = capabilities,
+		})
 	end
-
-	--https://github.com/aca/emmet-ls
-	lspconfig.emmet_ls.setup({
-		filetypes = { "html", "css", "template", "javascript" },
-	})
-end
-
-return M
+}
