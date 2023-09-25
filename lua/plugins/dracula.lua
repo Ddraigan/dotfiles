b8e5beefefe2b90fe15aa551c437b2514a366aef
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-09-25 14:46:41 +0100

    Theme Changes

--- lua/plugins/dracula.lua
+++ lua/plugins/dracula.lua
@@ -3,22 +3,54 @@ return {
 	lazy = false,
 	config = function()
 		local dracula = require("dracula")
+		local colors = {
+			bg = "#282A36",
+			fg = "#F8F8F2",
+			selection = "#44475A",
+			comment = "#6272A4",
+			red = "#FF5555",
+			orange = "#FFB86C",
+			yellow = "#F1FA8C",
+			green = "#50fa7b",
+			purple = "#BD93F9",
+			cyan = "#8BE9FD",
+			pink = "#FF79C6",
+			bright_red = "#FF6E6E",
+			bright_green = "#69FF94",
+			bright_yellow = "#FFFFA5",
+			bright_blue = "#D6ACFF",
+			bright_magenta = "#FF92DF",
+			bright_cyan = "#A4FFFF",
+			bright_white = "#FFFFFF",
+			menu = "#21222C",
+			visual = "#3E4452",
+			gutter_fg = "#4B5263",
+			nontext = "#3B4048",
+			white = "#ABB2BF",
+			black = "#191A21",
+		}
 		dracula.setup({
+			colors = colors,
 			-- customize dracula color palette
 			-- show the '~' characters after the end of buffers
 			show_end_of_buffer = true, -- default false
 			-- use transparent background
 			transparent_bg = true, -- default false
+			lualine_bg_color = colors.menu, -- default nil
 			-- set italic comment
 			italic_comment = true, -- default false
 			-- overrides the default highlights with table see `:h synIDattr`
-			overrides = {},
 			-- You can use overrides as table like this
 			-- overrides = {
 			--   NonText = { fg = "white" }, -- set NonText fg to white
 			--   NvimTreeIndentMarker = { link = "NonText" }, -- link to NonText highlight
 			--   Nothing = {} -- clear highlight of Nothing
 			-- },
+			--[[ overrides = function(colors)
+				return {
+					NonText = { fg = colors.white },
+				}
+			end, ]]
 			-- Or you can also use it like a function to get color from theme
 			-- overrides = function (colors)
 			--   return {
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,62 +1,59 @@
-local M = {
+return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = {
-		"nvim-tree/nvim-web-devicons", opt = true
+		"nvim-tree/nvim-web-devicons",
+		opt = true,
 	},
 	event = "VeryLazy",
-
-}
-
-M.config = function ()
-	require('lualine').setup {
-		options = {
-			icons_enabled = false,
-			theme = 'auto',
-			component_separators = '|',
-			section_separators = '',
-			disabled_filetypes = {
-				statusline = {},
-				winbar = {},
-			},
-			ignore_focus = {},
-			always_divide_middle = true,
-			globalstatus = false,
-			refresh = {
-				statusline = 1000,
-				tabline = 1000,
-				winbar = 1000,
-			}
-		},
-		sections = {
-			lualine_a = {'mode'},
-			lualine_b = {'branch', 'diff', 'diagnostics'},
-			lualine_c = {'filename'},
-			lualine_x = {
-				{
-					require("lazy.status").updates,
-					cond = require("lazy.status").has_updates,
-					color = { fg = "#ff9e64" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = false,
+				theme = "dracula-nvim",
+				component_separators = "|",
+				section_separators = "",
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
 				},
-				'encoding',
-				'fileformat',
-				'filetype'
+				ignore_focus = {},
+				always_divide_middle = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				},
 			},
-			lualine_y = {'progress'},
-			lualine_z = {'location'}
-		},
-		inactive_sections = {
-			lualine_a = {},
-			lualine_b = {},
-			lualine_c = {'filename'},
-			lualine_x = {'location'},
-			lualine_y = {},
-			lualine_z = {}
-		},
-		tabline = {},
-		winbar = {},
-		inactive_winbar = {},
-		extensions = {}
-	}
-end
-
-return M
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = {
+					{
+						require("lazy.status").updates,
+						cond = require("lazy.status").has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					"encoding",
+					"fileformat",
+					"filetype",
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = {},
+		})
+	end,
+}
