72067c3f1f29708ba346413dd86c5081233a5380
Ddraigan <lkjjones1999@gmail.com>
2023-11-09 23:53:15 +0000

    lualine icons

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				icons_enabled = false,
+				icons_enabled = true,
 				theme = "dracula-nvim",
 				component_separators = "|",
 				section_separators = "",
