925aaeb4076a8e352b62379cf4a0b5362a3579f4
Ddraigan <lkjjones1999@gmail.com>
2023-11-09 19:42:30 +0000

    mason auto install

--- lua/lsp/mason-tools-installer.lua
+++ lua/lsp/mason-tools-installer.lua
@@ -23,7 +23,7 @@ return {
 	},
 	config = function(_, opts)
 		local mason_tool_installer = require("mason-tool-installer")
-		mason_tool_installer.setup({ opts })
+		mason_tool_installer.setup(opts)
 		mason_tool_installer.run_on_start()
 	end,
 }
