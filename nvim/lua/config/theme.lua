d6fc4572ac37acb035b699f8f417114bf51774c2
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-11-10 16:51:14 +0000

    theme changes

--- nvim/lua/config/theme.lua
+++ nvim/lua/config/theme.lua
@@ -2,7 +2,18 @@ local M = {}
 
 M.colours = {
 	bg = "#282A36",
+	fg = "#F8F8F2",
+
 	black = "#191A21",
+	cyan = "#8BE9FD",
+	green = "#50fa7b",
+	orange = "#FFB86C",
+	pink = "#FF79C6",
+	purple = "#BD93F9",
+	red = "#FF5555",
+	white = "#ABB2BF",
+	yellow = "#F1FA8C",
+
 	bright_blue = "#D6ACFF",
 	bright_cyan = "#A4FFFF",
 	bright_green = "#69FF94",
@@ -10,22 +21,13 @@ M.colours = {
 	bright_red = "#FF6E6E",
 	bright_white = "#FFFFFF",
 	bright_yellow = "#FFFFA5",
+
 	comment = "#6272A4",
-	cyan = "#8BE9FD",
-	fg = "#F8F8F2",
-	green = "#50fa7b",
-	gutter_fg = "#4B5263",
-	innerbg = nil,
-	menu = "#21222C",
-	nontext = "#3B4048",
-	orange = "#FFB86C",
-	pink = "#FF79C6",
-	purple = "#BD93F9",
-	red = "#FF5555",
 	selection = "#44475A",
+	menu = "#21222C",
 	visual = "#3E4452",
-	white = "#ABB2BF",
-	yellow = "#F1FA8C",
+	gutter_fg = "#4B5263",
+	nontext = "#3B4048",
 }
 
 return M
