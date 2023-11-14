0a624d2b24b6570085766025e245e4924be0ea1f
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-11-14 14:58:45 +0000

    Moved on_attach to utils for reuse

--- nvim/lua/config/utils.lua
+++ nvim/lua/config/utils.lua
@@ -15,4 +15,23 @@ M.dump = function(o)
 	end
 end
 
+local function opts(desc, bufnr)
+	return { desc = "[LSP]: " .. desc, buffer = bufnr, noremap = true, silent = true }
+end
+
+M.on_attach = function(client, bufnr)
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Go To Declaration", bufnr))
+	-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go To Definition", bufnr))
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Hover", bufnr))
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Go To Implementation", bufnr))
+	vim.keymap.set("n", "ga", vim.lsp.buf.code_action, opts("Code Action", bufnr))
+	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts("Rename", bufnr))
+	-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts("Go To References", bufnr))
+	vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, opts("Go To Next Diagnostic", bufnr))
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts("Code Actions", bufnr))
+	--[[ vim.keymap.set("n", "<space>fM", function()
+					vim.lsp.buf.format({ async = true })
+				end, opts("Format", bufnr)) ]]
+end
+
 return M
--- nvim/lua/lsp/nvim-lspconfig.lua
+++ nvim/lua/lsp/nvim-lspconfig.lua
@@ -11,27 +11,9 @@ return {
 	config = function()
 		local lspconfig = require("lspconfig")
 		local mason_lspconfig = require("mason-lspconfig")
+		local on_attach = require("config.utils").on_attach
 		local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 		capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-		local function opts(desc, bufnr)
-			return { desc = "[LSP]: " .. desc, buffer = bufnr, noremap = true, silent = true }
-		end
-
-		local on_attach = function(client, bufnr)
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Go To Declaration", bufnr))
-			-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go To Definition", bufnr))
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Hover", bufnr))
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Go To Implementation", bufnr))
-			vim.keymap.set("n", "ga", vim.lsp.buf.code_action, opts("Code Action", bufnr))
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts("Rename", bufnr))
-			-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts("Go To References", bufnr))
-			vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, opts("Go To Next Diagnostic", bufnr))
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts("Code Actions", bufnr))
-			--[[ vim.keymap.set("n", "<space>fM", function()
-					vim.lsp.buf.format({ async = true })
-				end, opts("Format", bufnr)) ]]
-		end
 
 		local installed_servers = mason_lspconfig.get_installed_servers()
 		local unconfigured_servers = {}
