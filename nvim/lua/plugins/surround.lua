2a7f42097b1e0ed51b675b6531f7bc79079fe1ea
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2024-02-21 10:14:38 +0000

    merged

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -2,11 +2,7 @@ return {
 	-- Easy surrounding bois changing, adding, deleting. e.g; '' to "" with cs'"
 	"kylechui/nvim-surround",
 	event = "BufReadPre",
-<<<<<<< HEAD
-	conifg = function()
-=======
 	config = function()
->>>>>>> d87e908c227a45bff47a08180d0f65134d63a472
 		require("nvim-surround").setup({})
 	end,
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -26,57 +26,6 @@ return {
 				"markdown_inline",
 				"astro",
 			},
-			textobjects = {
-				select = {
-					enable = true,
-<<<<<<< HEAD
-					lookahead = true,
-=======
-
-					-- Automatically jump forward to textobj, similar to targets.vim
-					lookahead = true,
-
->>>>>>> d87e908c227a45bff47a08180d0f65134d63a472
-					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-<<<<<<< HEAD
-						["ic"] = "@class.inner",
-					},
-=======
-						-- You can optionally set descriptions to the mappings (used in the desc parameter of
-						-- nvim_buf_set_keymap) which plugins like which-key display
-						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-						-- You can also use captures from other query groups like `locals.scm`
-						["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-					},
-					-- You can choose the select mode (default is charwise 'v')
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * method: eg 'v' or 'o'
-					-- and should return the mode ('v', 'V', or '<c-v>') or a table
-					-- mapping query_strings to modes.
-					selection_modes = {
-						["@parameter.outer"] = "v", -- charwise
-						["@function.outer"] = "V", -- linewise
-						["@class.outer"] = "<c-v>", -- blockwise
-					},
-					-- If you set this to `true` (default is `false`) then any textobject is
-					-- extended to include preceding or succeeding whitespace. Succeeding
-					-- whitespace has priority in order to act similarly to eg the built-in
-					-- `ap`.
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * selection_mode: eg 'v'
-					-- and should return true of false
-					include_surrounding_whitespace = true,
->>>>>>> d87e908c227a45bff47a08180d0f65134d63a472
-				},
-			},
 			sync_install = false,
 			auto_install = true,
 			highlight = {
@@ -89,6 +38,25 @@ return {
 					node_incremental = "<CR>",
 					scope_incremental = "<S-CR>",
 					node_decremental = "<BS>",
+				},
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+					["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+					["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+				},
+				selection_modes = {
+					["@parameter.outer"] = "v", -- charwise
+					["@function.outer"] = "V", -- linewise
+					["@class.outer"] = "<c-v>", -- blockwise
 				},
 			},
 		})
