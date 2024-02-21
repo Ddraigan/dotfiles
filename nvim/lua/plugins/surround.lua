ada49f54f30149b9673ba68a54c5d61239cc6c69
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2024-02-21 10:07:16 +0000

    merge time

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -2,4 +2,7 @@ return {
 	-- Easy surrounding bois changing, adding, deleting. e.g; '' to "" with cs'"
 	"tpope/vim-surround",
 	event = "BufReadPre",
+	conifg = function()
+		require("nvim-surround").setup({})
+	end,
 }
