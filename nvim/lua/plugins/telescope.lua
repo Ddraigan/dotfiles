3c535ae61a4eae1780e584843f7c15ed610ba099
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2024-02-26 12:40:01 +0000

    telescope changes

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -49,6 +49,34 @@ return {
 					preview_cutoff = 50,
 				},
 				file_ignore_patterns = { "node_modules" },
+				preview = {
+					mime_hook = function(filepath, bufnr, opts)
+						local is_image = function(filepath)
+							local image_extensions = { "png", "jpg" } -- Supported image formats
+							local split_path = vim.split(filepath:lower(), ".", { plain = true })
+							local extension = split_path[#split_path]
+							return vim.tbl_contains(image_extensions, extension)
+						end
+						if is_image(filepath) then
+							local term = vim.api.nvim_open_term(bufnr, {})
+							local function send_output(_, data, _)
+								for _, d in ipairs(data) do
+									vim.api.nvim_chan_send(term, d .. "\r\n")
+								end
+							end
+							vim.fn.jobstart({
+								"catimg",
+								filepath, -- Terminal image viewer command
+							}, { on_stdout = send_output, stdout_buffered = true, pty = true })
+						else
+							require("telescope.previewers.utils").set_preview_message(
+								bufnr,
+								opts.winid,
+								"Binary cannot be previewed"
+							)
+						end
+					end,
+				},
 			},
 			pickers = {
 				lsp_references = {
@@ -59,17 +87,21 @@ return {
 				windowizer = {
 					find_cmd = "fd", -- find command. Available options [ find | fd | rg ] (defaults to "fd")
 				},
-				fzf = {
-					fuzzy = true,
-					override_generic_sorter = true,
-					override_file_sorter = true,
-					case_mode = "smart_case",
+				extensions = {
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
 				},
 			},
 		}
+
 		require("telescope").setup(opts)
 		require("telescope").load_extension("windowizer")
 		require("telescope").load_extension("fzf")
+
 		-- require("telescope").load_extension("harpoon")
 	end,
 }
