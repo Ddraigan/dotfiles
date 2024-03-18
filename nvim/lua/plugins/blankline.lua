9e09159bc7216eae7fbd4b6778d1128b41327b5e
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2024-03-18 14:28:02 +0000

    blankline changes

--- nvim/lua/plugins/blankline.lua
+++ nvim/lua/plugins/blankline.lua
@@ -3,17 +3,36 @@ return {
     lazy = false,
     main = "ibl",
     config = function ()
-        -- local colour = require("config.theme").colours
-        -- local hooks = require("ibl.hooks")
-        -- create the highlight groups in the highlight setup hook, so they are reset
-        -- every time the colorscheme changes
-        -- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        -- 	vim.api.nvim_set_hl(0, "BrightGreen", { fg = colour.bright_green })
+        -- local colour = require("config.theme").soft
+
+        -- local highlight = {
+        --     "RainbowRed",
+        --     "RainbowYellow",
+        --     "RainbowBlue",
+        --     "RainbowOrange",
+        --     "RainbowGreen",
+        --     "RainbowViolet",
+        --     "RainbowCyan",
+        -- }
+
+        -- local hooks = require "ibl.hooks"
+        -- -- create the highlight groups in the highlight setup hook, so they are reset
+        -- -- every time the colorscheme changes
+        -- hooks.register(hooks.type.HIGHLIGHT_SETUP, function ()
+        --     vim.api.nvim_set_hl(0, "RainbowRed", { fg = colour.bright_red })
+        --     vim.api.nvim_set_hl(0, "RainbowYellow", { fg = colour.bright_yellow })
+        --     vim.api.nvim_set_hl(0, "RainbowBlue", { fg = colour.bright_blue })
+        --     vim.api.nvim_set_hl(0, "RainbowOrange", { fg = colour.orange })
+        --     vim.api.nvim_set_hl(0, "RainbowGreen", { fg = colour.bright_green })
+        --     vim.api.nvim_set_hl(0, "RainbowViolet", { fg = colour.bright_magenta })
+        --     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = colour.bright_cyan })
         -- end)
+
         require("ibl").setup({
-            scope = { enabled = true },
+            -- scope = { enabled = true },
             -- indent = { highlight = "BrightGreen" },
             indent = {
+                -- highlight = highlight,
                 char = "┊",
                 tab_char = "┊",
                 smart_indent_cap = true,
