45bd364196343cf78b38d1c0bdb142d846442dd0
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2024-03-19 13:08:25 +0000

    git

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,18 +1,19 @@
 return {
-	-- Git DiffView
-	{ "sindrets/diffview.nvim" },
+    -- Git DiffView
+    { "sindrets/diffview.nvim" },
 
-	-- Git from command line support
-	{ "tpope/vim-fugitive", cmd = "Git" }, -- :Git to use git in nvim
-	--  { 'tpope/vim-rhubarb', event = "VeryLazy" },
+    -- Git from command line support
+    { "tpope/vim-fugitive",    cmd = "Git" }, -- :Git to use git in nvim
+    --  { 'tpope/vim-rhubarb', event = "VeryLazy" },
 
-	-- Git Diff and signs
-	{
-		"lewis6991/gitsigns.nvim",
-		event = "BufReadPre",
-		dependencies = {
-			{ "sindrets/diffview.nvim", cmd = "DiffviewOpen" },
-		},
-		config = true,
-	},
+    -- Git Diff and signs
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "BufReadPre",
+
+        dependencies = {
+            { "sindrets/diffview.nvim", cmd = "DiffviewOpen" },
+        },
+        config = true,
+    },
 }
