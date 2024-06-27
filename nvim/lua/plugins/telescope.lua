0e411b5afbee1f590b03c5999dfdda7a96c15571
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2024-06-27 16:43:23 +0100

    fix telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -15,10 +15,6 @@ return {
           i = { ["<c-t>"] = require("trouble.sources.telescope").open },
           n = { ["<c-t>"] = require("trouble.sources.telescope").open },
         },
-        get_selection_window = function()
-          require("edgy").goto_main()
-          return 0
-        end,
         vimgrep_arguments = {
           "rg",
           "--color=never",
