114dae38bf3905bd5df8e3aab60cc0f3fba2d25a
Ddraigan <lkjjones1999@gmail.com>
2024-08-31 11:57:25 +0100

    some config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require("wezterm")
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.color_scheme = "Catppuccin Mocha"
+config.enable_tab_bar = false
+config.automatically_reload_config = true
+config.window_background_opacity = 1
+config.window_background_image = "/home/leon/Pictures/background.jpg"
+config.window_background_image_hsb = {
+	hue = 1.0,
+	saturation = 1.0,
+	brightness = 0.03,
+}
+
+return config
