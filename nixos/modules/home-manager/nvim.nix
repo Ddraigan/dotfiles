837e703b17c43ed027a55660e1f6b5e1be84d09d
Ddraigan <lkjjones1999@gmail.com>
2024-09-09 18:57:19 +0100

    nvim using nix !

--- nixos/modules/home-manager/nvim.nix
+++ nixos/modules/home-manager/nvim.nix
@@ -1,13 +1,4 @@
 { pkgs, outputs, lib, config, ... }: {
-  nixpkgs = {
-    overlays = [ outputs.overlays.unstable-packages ];
-
-    # Config for nixpkgs instance
-    config = {
-      allowUnfree = true;
-      allowUnfreePredicate = _: true;
-    };
-  };
   config = {
     home.packages = [
       pkgs.unstable.neovim
