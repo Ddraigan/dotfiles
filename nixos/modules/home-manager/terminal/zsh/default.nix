d76ab8104e55de4a21ed1fd112ce2e47f14f59eb
Ddraigan <lkjjones1999@gmail.com>
2025-03-05 20:20:18 +0000

    fuck uwsm

--- nixos/modules/home-manager/terminal/zsh/default.nix
+++ nixos/modules/home-manager/terminal/zsh/default.nix
@@ -21,15 +21,15 @@
         #   tmux new -As default
         # fi
 
-        # Use this to bring up the uwsm compositor menu
-        if uwsm check may-start && uwsm select; then
-        	exec systemd-cat -t uwsm_start uwsm start default
-        fi
       '';
     };
   };
 }
 
+        # # Use this to bring up the uwsm compositor menu
+        # if uwsm check may-start && uwsm select; then
+        # 	exec systemd-cat -t uwsm_start uwsm start default
+        # fi
 
 # Use this to go straight into hyprland
 #  if uwsm check may-start; then
