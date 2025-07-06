2add9c2b767ca84549853d0a02a9935d7a7d22a4
Ddraigan <lkjjones1999@gmail.com>
2025-07-06 12:24:36 +0100

    switch nvim to stable

--- nixos/modules/home-manager/terminal/nvim/default.nix
+++ nixos/modules/home-manager/terminal/nvim/default.nix
@@ -1,10 +1,13 @@
-{ pkgs, lib, config, ... }:
-
 {
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   options.modules.terminal.nvim.enable = lib.mkEnableOption "Enable Nvim";
   config = lib.mkIf config.modules.terminal.nvim.enable {
     home.packages = [
-      pkgs.unstable.neovim
+      pkgs.neovim
     ];
     home.file = {
       ".config/nvim" = {
