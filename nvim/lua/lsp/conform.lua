7e651120f8ce4fdc1328dfc2362338788ef16d51
Ddraigan <lkjjones1999@gmail.com>
2025-08-21 19:57:04 +0100

    fixed formatting on nix

--- nvim/lua/lsp/conform.lua
+++ nvim/lua/lsp/conform.lua
@@ -8,6 +8,11 @@ return {
       lua = { "stylua" },
       javascript = { "prettierd", "prettier", stop_after_first = true },
       typescript = { "prettierd", "prettier", stop_after_first = true },
+      astro = { "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+      html = { "prettierd", "prettier", stop_after_first = true },
+      css = { "prettierd", "prettier", stop_after_first = true },
       nix = { "alejandra" },
       -- Use the "_" filetype to run formatters on filetypes that don't
       -- have other formatters configured.
