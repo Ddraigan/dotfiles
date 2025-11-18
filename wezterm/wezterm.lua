4d952f3dd3dde26ad697d42d7ca459fa0d724482
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2025-11-18 11:36:08 +0000

    buncha cleanup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,15 +5,14 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.enable_wayland = false;
+config.enable_wayland = false
 config.color_scheme = "Catppuccin Mocha"
 config.colors = {
-	background = 'transparent'
+	background = "transparent",
 }
 config.enable_tab_bar = false
 config.automatically_reload_config = true
 config.window_background_opacity = 0
--- config.window_background_image = "/home/leon/Pictures/leavesbg.jpg"
 config.window_background_image_hsb = {
 	hue = 1.0,
 	saturation = 1.0,
