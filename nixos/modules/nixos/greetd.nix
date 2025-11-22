dddbb8f8a64ba4d941154dc92e4efd56a622c99a
Ddraigan <lkjjones1999@gmail.com>
2025-11-22 17:42:03 +0000

    maybe greet again

--- nixos/modules/nixos/greetd.nix
+++ nixos/modules/nixos/greetd.nix
@@ -7,7 +7,7 @@
   options.modules.nix.greetd.enable = lib.mkEnableOption "Enable greetd";
   config = lib.mkIf config.modules.nix.greetd.enable (let
     hyprGreetConf = pkgs.writeText "hypr-greet.conf" ''
-      exec-once = regreet; hyprctl dispatch exit
+      exec-once = "${config.programs.regreet.package}/bin/regreet"; hyprctl dispatch exit
       misc {
           disable_hyprland_logo = true
           disable_splash_rendering = true
@@ -21,7 +21,8 @@
       restart = false;
       settings = {
         initial_session = {
-          command = "Hyprland --config ${hyprGreetConf}";
+          # command = "${config.programs.hyprland.package}/bin/Hyprland --config ${hyprGreetConf}";
+          command = "uswm start hyprland.desktop --config ${hyprGreetConf}";
           user = "greeter";
         };
         default_session = {
