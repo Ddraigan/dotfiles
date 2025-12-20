128cba6cc910a5d730d8f11a0d637d345dda8c8b
Ddraigan <lkjjones1999@gmail.com>
2025-12-20 17:24:36 +0000

    doesn't work

--- nixos/modules/home-manager/desktop/lutris/default.nix
+++ nixos/modules/home-manager/desktop/lutris/default.nix
@@ -2,18 +2,25 @@
   pkgs,
   lib,
   config,
+  nixosConfig,
   ...
 }: {
   options.modules.desktop.lutris.enable = lib.mkEnableOption "Enable Lutris Game Launcher";
 
-  config =
-    lib.mkIf config.modules.desktop.lutris.enable {
-      programs.lutris = {
-        enable = true;
-        # steamPackage = pkgs.steam;
-        protonPackages = [
-          pkgs.proton-ge-bin
-        ];
-      };
+  config = lib.mkIf config.modules.desktop.lutris.enable {
+    programs.lutris = {
+      enable = true;
+      steamPackage = nixosConfig.programs.steam.package;
+      extraPackages = with pkgs; [
+        mangohud
+        winetricks
+        gamescope
+        gamemode
+        umu-launcher
+      ];
+      protonPackages = [
+        pkgs.proton-ge-bin
+      ];
     };
+  };
 }
