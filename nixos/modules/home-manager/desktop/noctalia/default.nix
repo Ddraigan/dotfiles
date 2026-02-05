861f0e4e6e89f63cb32dde42801cb1ebb48add27
Leon Jones <Leon@rnw.local>
2026-02-05 17:16:38 +0000

    noctalia

--- nixos/modules/home-manager/desktop/noctalia/default.nix
+++ nixos/modules/home-manager/desktop/noctalia/default.nix
@@ -10,26 +10,45 @@ in
     enable = lib.mkEnableOption "Enable Noctalia Shell";
   };
   config = lib.mkIf cfg.enable {
-    enable = true;
-    colors = let
-      hex = colours.hex;
-    in  {
-      mError = hex.red;
-      mOnError = hex.text;
-      mOnPrimary = hex.text;
-      mOnSecondary = hex.text;
-      mOnSurface = hex.text;
-      mOnSurfaceVariant = hex.text;
-      mOnTertiary = hex.text;
-      mOnHover = hex.text;
-      mOutline = hex.crust;
-      mPrimary = hex.mauve;
-      mSecondary = hex.maroon;
-      mShadow = hex.crust;
-      mSurface = hex.surface0;
-      mHover = hex.base;
-      mSurfaceVariant = hex.surface3;
-      mTertiary = hex.pink;
+    programs.noctalia-shell ={
+      enable = true;
+      systemd.enable = true;
+      plugins = {
+        sources = [
+        {
+          enabled = true;
+          name = "Official Noctalia Plugins";
+          url = "https://github.com/noctalia-dev/noctalia-plugins";
+        }
+        ];
+        states = {
+          catwalk = {
+            enabled = true;
+            sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+          };
+        };
+        version = 1;
+      };
+      colors = let
+        hex = colours.hex;
+      in  {
+        mError = hex.red;
+        mOnError = hex.text;
+        mOnPrimary = hex.text;
+        mOnSecondary = hex.text;
+        mOnSurface = hex.text;
+        mOnSurfaceVariant = hex.text;
+        mOnTertiary = hex.text;
+        mOnHover = hex.text;
+        mOutline = hex.crust;
+        mPrimary = hex.mauve;
+        mSecondary = hex.maroon;
+        mShadow = hex.crust;
+        mSurface = hex.surface0;
+        mHover = hex.base;
+        mSurfaceVariant = hex.surface3;
+        mTertiary = hex.pink;
+      };
     };
   };
 }
