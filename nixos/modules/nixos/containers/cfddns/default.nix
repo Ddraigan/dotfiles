5b35d58b4c6143c2135c12f96cee49463d422507
Ddraigan <lkjjones1999@gmail.com>
2026-02-17 23:39:08 +0000

    cfddns

---
+++ nixos/modules/nixos/containers/cfddns/default.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  mainUser = "leon";
+  dataPath = "${config.users.users.${mainUser}.home}/appdata";
+  cfddnsPath = "${dataPath}/cloudflare-ddns";
+in {
+  config = {
+    systemd.tmpfiles.rules = [
+      "d ${cfddnsPath} 0755 ${mainUser} users -"
+    ];
+
+    virtualisation.oci-containers.containers.cloudflare-ddns = {
+      image = "favonia/cloudflare-ddns:latest";
+
+      # Use host networking to make IPv6 easier
+      networkMode = "host";
+
+      restartPolicy = "always";
+
+      user = "1000:1000";
+
+      readOnly = true;
+
+      # Drop all Linux capabilities and restrict privileges
+      capDrop = ["all"];
+      securityOpt = ["no-new-privileges:true"];
+
+      environmentFiles = [
+        "/home/leon/secrets/cloudflare-ddns.env"
+      ];
+
+      volumes = [
+        "${cfddnsPath}:/data"
+      ];
+    };
+  };
+}
