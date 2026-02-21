28aa999a255a1885670f95a064cf8f0b88fdde51
Ddraigan <lkjjones1999@gmail.com>
2026-02-21 18:09:29 +0000

    fix

--- nixos/modules/nixos/containers/traefik/default.nix
+++ nixos/modules/nixos/containers/traefik/default.nix
@@ -66,7 +66,7 @@ in {
             "--providers.docker=true"
             "--entrypoints.web.address=:80"
             "--entrypoints.websecure.address=:443"
-            # "--certificatesresolvers.certresolver.acme.email=CF_API_EMAIL"
+            "--certificatesresolvers.certresolver.acme.email=$CF_API_EMAIL"
             "--certificatesresolvers.certresolver.acme.storage=/data/acme.json"
             "--certificatesresolvers.certresolver.acme.dnschallenge=true"
             "--certificatesresolvers.certresolver.acme.dnschallenge.provider=cloudflare"
@@ -78,40 +78,3 @@ in {
     };
   };
 }
-# virtualisation.oci-containers.containers = {
-#   traefik = {
-#     image = "traefik:latest";
-#     autoStart = true;
-#     ports = [
-#       "80:80"
-#       "443:443"
-#     ];
-#     volumes = [
-#       "/etc/localtime:/etc/localtime:ro"
-#       "/var/run/docker.sock:/var/run/docker.sock:ro"
-#       "/etc/traefik/traefik.yml:/traefik.yml:ro"
-#       "/etc/traefik_dynamic.yml:/traefik_dynamic.yml:ro"
-#       "/srv/traefik/acme.json:/acme.json"
-#     ];
-#     environment = {
-#       CF_API_EMAIL = "";
-#       CF_API_TOKEN = "";
-#     };
-#     # environmentFiles = [
-#     #   "/run/secrets/traefik.env"
-#     # ];
-#     labels = {
-#       "traefik.enable" = "true";
-#       "traefik.http.routers.api.entrypoints" = "https";
-#       "traefik.http.routers.api.rule" = "Host(`traefik.ddraigan.com`)";
-#       "traefik.http.routers.api.service" = "api@internal";
-#     };
-#     extraOptions = [
-#       "--network=traefikProxy"
-#       # "--restart=unless-stopped"
-#       "--security-opt=no-new-privileges:true"
-#       "--name=traefik"
-#     ];
-#   };
-# };
-
