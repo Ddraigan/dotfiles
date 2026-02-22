d4c5484cec0b49c8072166d9f3978374021d27ac
Ddraigan <lkjjones1999@gmail.com>
2026-02-22 11:44:03 +0000

    new name

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -114,7 +114,7 @@
       leon-pc = mkMachine "leon-pc" "x86_64-linux";
       leon-laptop = mkMachine "leon-laptop" "x86_64-linux";
       leon-dell = mkMachine "leon-dell" "x86_64-linux";
-      srv-dell = mkMachine "srv-dell" "x86_64-linux";
+      mynydd = mkMachine "mynydd" "x86_64-linux";
       iso = mkIso "iso" "x86_64-linux";
     };
 
