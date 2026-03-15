258a965f4ec5fe3e26ae9297e8d4b1311d42ad5d
Ddraigan <lkjjones1999@gmail.com>
2026-03-15 21:17:37 +0000

    revert

--- nixos/modules/nixos/containers/traefik/default.nix
+++ nixos/modules/nixos/containers/traefik/default.nix
@@ -28,35 +28,39 @@ in {
           volumes = [
             "/var/run/docker.sock:/var/run/docker.sock"
             "${traefikPath}:/data"
-            "${import ./traefikconf.nix {inherit pkgs config containerUtils;}}:/etc/traefik/traefik.yaml:ro"
+            # "${import ./traefikconf.nix {inherit pkgs config containerUtils;}}:/etc/traefik/traefik.yaml:ro"
           ];
           environmentFiles = [
             "/home/leon/secrets/traefik.env"
           ];
           labels = {
             "traefik.enable" = "true";
+
+            # Dashboard Router
             "traefik.http.routers.traefik-dashboard.rule" = "Host(`traefik.${cfg.domain}`)";
             "traefik.http.routers.traefik-dashboard.entrypoints" = "websecure";
             "traefik.http.routers.traefik-dashboard.service" = "api@internal";
+            "traefik.http.services.traefik-dashboard.loadbalancer.server.port" = "8080";
+
+            # SSL Wildcard Config
             "traefik.http.routers.traefik-dashboard.tls" = "true";
+            "traefik.http.routers.traefik-dashboard.tls.certresolver" = "certresolver";
             "traefik.http.routers.traefik-dashboard.tls.domains[0].main" = "${cfg.domain}";
             "traefik.http.routers.traefik-dashboard.tls.domains[0].sans" = "*.${cfg.domain}";
-            "traefik.http.services.traefik-dashboard.loadbalancer.server.port" = "8080";
-            # "traefik.http.routers.traefik-dashboard.tls.certresolver" = "certresolver";
           };
-          # cmd = [
-          #   "--api.insecure=true"
-          #   "--providers.docker=true"
-          #   "--entrypoints.web.address=:80"
-          #   "--entrypoints.web.http.redirections.entrypoint.to=websecure"
-          #   "--entrypoints.web.http.redirections.entrypoint.scheme=https"
-          #   "--entrypoints.websecure.address=:443"
-          #
-          #   "--certificatesresolvers.certresolver.acme.email=lkjjones1999@gmail.com"
-          #   "--certificatesresolvers.certresolver.acme.storage=/data/acme.json"
-          #   "--certificatesresolvers.certresolver.acme.dnschallenge=true"
-          #   "--certificatesresolvers.certresolver.acme.dnschallenge.provider=cloudflare"
-          # ];
+          cmd = [
+            "--api.insecure=true"
+            "--providers.docker=true"
+            "--entrypoints.web.address=:80"
+            "--entrypoints.web.http.redirections.entrypoint.to=websecure"
+            "--entrypoints.web.http.redirections.entrypoint.scheme=https"
+            "--entrypoints.websecure.address=:443"
+
+            "--certificatesresolvers.certresolver.acme.email=lkjjones1999@gmail.com"
+            "--certificatesresolvers.certresolver.acme.storage=/data/acme.json"
+            "--certificatesresolvers.certresolver.acme.dnschallenge=true"
+            "--certificatesresolvers.certresolver.acme.dnschallenge.provider=cloudflare"
+          ];
         };
       };
     };
