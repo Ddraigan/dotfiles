932e244e03ccffc52fed10c1967232b0fcbedb9f
Leon Jones <Leon@rnw.local>
2026-03-23 16:19:23 +0000

    cleanup comments

--- nvim/lua/lsp/blink-pairs.lua
+++ nvim/lua/lsp/blink-pairs.lua
@@ -1,15 +1,8 @@
 return {
   'saghen/blink.pairs',
   version = '*', -- (recommended) only required with prebuilt binaries
-
   -- download prebuilt binaries from github releases
   dependencies = 'saghen/blink.download',
-  -- OR build from source, requires nightly:
-  -- https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
-
   --- @module 'blink.pairs'
   --- @type blink.pairs.Config
   opts = {
@@ -22,8 +15,6 @@ return {
       -- or disable with `vim.g.pairs = false` (global) and `vim.b.pairs = false` (per-buffer)
       -- and/or with `vim.g.blink_pairs = false` and `vim.b.blink_pairs = false`
       disabled_filetypes = {},
-      -- see the defaults:
-      -- https://github.com/Saghen/blink.pairs/blob/main/lua/blink/pairs/config/mappings.lua#L14
       pairs = {},
     },
     highlights = {
