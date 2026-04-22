c9657356c74ed394d33c9dcc082411af0bf87933
Leon Jones <Leon@rnw.local>
2026-04-22 10:15:50 +0100

    possible extras

--- nixos/modules/dendritic.nix
+++ nixos/modules/dendritic.nix
@@ -1,20 +1,34 @@
 {inputs, ...}: {
-  # flake-file.inputs = {
-  #   nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  #   flake-parts.url = "github:hercules-ci/flake-parts";
-  #   flake-file.url = lib.mkDefault "github:vic/flake-file";
-  #   import-tree.url = "github:vic/import-tree";
-  # };
-  flake-file.inputs.home-manager = {
-    url = "github:nix-community/home-manager";
-    inputs.nixpkgs.follows = "nixpkgs";
+  flake-file.inputs = {
+    #   nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # flake-parts = {
+    #   url = "github:hercules-ci/flake-parts";
+    #   inputs.nixpkgs-lib.follows = "nixpkgs";
+    # };
+    #   flake-file.url = lib.mkDefault "github:vic/flake-file";
+    #   import-tree.url = "github:vic/import-tree";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-
-  # flake-file.type = "dendritic";
-  # flake-file.path = ./.;
 
   imports = [
     # inputs.flake-parts.flakeModules.modules
+    # inputs.flake-file.flakeModules.default
     inputs.flake-file.flakeModules.dendritic
   ];
+
+  # import all modules recursively with import-tree
+  # flake-file.outputs = ''
+  #   inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules)
+  # '';
+
+  # set flake.systems
+  # systems = [
+  #   "aarch64-darwin"
+  #   "aarch64-linux"
+  #   "x86_64-darwin"
+  #   "x86_64-linux"
+  # ];
 }
