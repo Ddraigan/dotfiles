93e45a5ff873e2b4452170cb5043b69a23d5da9b
Ddraigan <lkjjones1999@gmail.com>
2026-09-22 21:40:16 +0100

    feat: gamescope with steamos alias

--- nixos/modules/cmdline/zsh.nix
+++ nixos/modules/cmdline/zsh.nix
@@ -6,6 +6,9 @@
     ...
   }: {
     config = {
+      home.shellAliases = {
+        steamos = "uwsm app -- gamescope -W 2560 -H 1440 -- steam -steamos3 -gamepadui -steamdeck -steampal";
+      };
       programs.zsh = {
         enable = true;
         syntaxHighlighting.enable = true;
