30c7b4a2102a663446e969456db13526b01dfc4d
Ddraigan <lkjjones1999@gmail.com>
2026-09-22 19:13:55 +0100

    fix brightness

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -269,7 +269,7 @@
                 new_optimizations = true;
                 xray = true;
                 ignore_opacity = true;
-                brightness = 0.3;
+                brightness = 0.1;
               };
             };
 
