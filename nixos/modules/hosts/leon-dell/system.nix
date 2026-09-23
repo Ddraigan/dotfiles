5572c2f68eacc3fb4ebc0c7eaa2e1c438790afbc
Ddraigan <lkjjones1999@gmail.com>
2026-09-23 15:32:06 +0000

    refactor modules

--- nixos/modules/hosts/leon-dell/system.nix
+++ nixos/modules/hosts/leon-dell/system.nix
@@ -19,4 +19,36 @@
         sunshine
       ]);
   };
+
+  flake.homeConfigurations.leon-dell = inputs.home-manager-unstable.lib.homeManagerConfiguration {
+    pkgs = import inputs.nixpkgs-unstable {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
+    extraSpecialArgs = {
+      inherit inputs;
+    };
+    modules =
+      [
+        ./_host/home.nix
+      ]
+      ++ (with config.homeManager; [
+        base
+        shared
+        uwsm
+        fonts
+        stylix
+        nvim
+        cmdline
+        wezterm
+        noctalia
+        hyprland
+        hyprlock
+        hypridle
+        nemo
+        spicetify
+        wlogout
+        zen
+      ]);
+  };
 }
--- nixos/modules/hosts/wsl-work/system.nix
+++ nixos/modules/hosts/wsl-work/system.nix
@@ -12,4 +12,26 @@
       ./_host/configuration.nix
     ];
   };
+
+  flake.homeConfigurations.wsl-work = inputs.home-manager-unstable.lib.homeManagerConfiguration {
+    pkgs = import inputs.nixpkgs-unstable {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
+    extraSpecialArgs = {
+      inherit inputs;
+    };
+    modules =
+      [
+        ./_host/home.nix
+      ]
+      ++ (with config.homeManager; [
+        base
+        shared
+        fonts
+        stylix
+        nvim
+        cmdline
+      ]);
+  };
 }
