a4d5a7e02df98046066c6112dc118746ca34cef6
Ddraigan <lkjjones1999@gmail.com>
2026-09-24 15:25:44 +0000

    settings style change

--- nixos/modules/machines/leon-pc/settings.nix
+++ nixos/modules/machines/leon-pc/settings.nix
@@ -6,18 +6,22 @@
   flake.nixosConfigurations.leon-pc = inputs.nixpkgs-unstable.lib.nixosSystem {
     system = "x86_64-linux";
     specialArgs = {inherit inputs;};
-    modules = with config.nixos; [
-      leon-pc
-      leon
-      cmdline
-      base
-      fonts
-      hyprland-de
-      greetd
-      nvidia
-      gaming
-      sunshine
-    ];
+    modules =
+      [
+        ./_settings/system.nix
+      ]
+      ++ (with config.nixos; [
+        leon-pc
+        leon
+        cmdline
+        base
+        fonts
+        hyprland-de
+        greetd
+        nvidia
+        gaming
+        sunshine
+      ]);
   };
 
   flake.homeConfigurations.leon = inputs.home-manager-unstable.lib.homeManagerConfiguration {
@@ -28,22 +32,26 @@
     extraSpecialArgs = {
       inherit inputs;
     };
-    modules = with config.homeManager; [
-      leon-pc-home-leon
-      leon
-      cmdline
-      base
-      hyprland-de
-      colours
-      fonts
-      stylix
-      nvim
-      wezterm
-      gaming
-      obs
-      nemo
-      spicetify
-      zen
-    ];
+    modules =
+      [
+        ./_settings/home-leon.nix
+      ]
+      ++ (with config.homeManager; [
+        leon-pc-home-leon
+        leon
+        cmdline
+        base
+        hyprland-de
+        colours
+        fonts
+        stylix
+        nvim
+        wezterm
+        gaming
+        obs
+        nemo
+        spicetify
+        zen
+      ]);
   };
 }
