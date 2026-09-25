2011b04a28dffee5d0d68c108d0e539b50e007ef
Ddraigan <lkjjones1999@gmail.com>
2026-09-26 00:02:40 +0100

    authentik

--- nixos/modules/containers/authentik.nix
+++ nixos/modules/containers/authentik.nix
@@ -1,103 +1,108 @@
 {...}: {
-  nixos.authentik =
-    {
-      lib,
-      pkgs,
-      config,
-      containerUtils,
-      ...
-    }:
-    let
-      cfg = config.modules.nix.containers;
-      authentikPath = containerUtils.mkDataPath "authentik";
-      secretsFile = "/home/${cfg.mainUser}/secrets/authentik.env";
-    in {
-      config = {
-    systemd.tmpfiles.rules = [
-      "d ${authentikPath} 0755 ${cfg.mainUser} users -"
-      "d ${authentikPath}/postgres 0755 ${cfg.mainUser} users -"
-      "d ${authentikPath}/data 0755 ${cfg.mainUser} users -"
-      "d ${authentikPath}/certs 0755 ${cfg.mainUser} users -"
-      "d ${authentikPath}/custom-templates 0755 ${cfg.mainUser} users -"
-    ];
+  nixos.authentik = {
+    lib,
+    pkgs,
+    config,
+    containerUtils,
+    ...
+  }: let
+    cfg = config.modules.nix.containers;
+    authentikPath = containerUtils.mkDataPath "authentik";
+    secretsFile = "/home/${cfg.mainUser}/secrets/authentik.env";
+  in {
+    config = {
+      systemd.tmpfiles.rules = [
+        "d ${authentikPath} 0755 ${cfg.mainUser} users -"
+        "d ${authentikPath}/postgres 0755 ${cfg.mainUser} users -"
+        "d ${authentikPath}/data 0755 ${cfg.mainUser} users -"
+        "d ${authentikPath}/certs 0755 ${cfg.mainUser} users -"
+        "d ${authentikPath}/custom-templates 0755 ${cfg.mainUser} users -"
+      ];
 
-    virtualisation.oci-containers.containers = {
-      authentik-postgres = {
-        image = "docker.io/library/postgres:16-alpine";
-        environment = {
-          POSTGRES_DB = "authentik";
-          POSTGRES_USER = "authentik";
+      virtualisation.oci-containers.containers = {
+        authentik-postgres = {
+          image = "docker.io/library/postgres:16-alpine";
+          environment = {
+            POSTGRES_DB = "authentik";
+            POSTGRES_USER = "authentik";
+          };
+          environmentFiles = [secretsFile];
+          volumes = [
+            "${authentikPath}/postgres:/var/lib/postgresql/data"
+          ];
+          extraOptions = [
+            "--health-cmd=pg_isready -U authentik -d authentik"
+            "--health-interval=10s"
+            "--health-timeout=5s"
+            "--health-retries=5"
+            "--health-start-period=20s"
+          ];
         };
-        environmentFiles = [secretsFile];
-        volumes = [
-          "${authentikPath}/postgres:/var/lib/postgresql/data"
-        ];
-        extraOptions = [
-          "--health-cmd=pg_isready -U authentik -d authentik"
-          "--health-interval=10s"
-          "--health-timeout=5s"
-          "--health-retries=5"
-          "--health-start-period=20s"
-        ];
-      };
 
-      authentik-server = {
-        image = "ghcr.io/goauthentik/server:2026.8.3";
-        cmd = ["server"];
-        dependsOn = ["authentik-postgres"];
-        networks = ["container:authentik-postgres"];
-        environment = {
-          AUTHENTIK_POSTGRESQL__HOST = "localhost";
-          AUTHENTIK_POSTGRESQL__NAME = "authentik";
-          AUTHENTIK_POSTGRESQL__USER = "authentik";
-          AUTHENTIK_WEB__BASE_URL = "https://authentik.${cfg.domain}";
-          TZ = config.time.timeZone;
-        };
-        environmentFiles = [secretsFile];
-        extraOptions = [
-          "--shm-size=512mb"
-        ];
-        volumes = [
-          "${authentikPath}/data:/data"
-          "${authentikPath}/custom-templates:/templates"
-        ];
-        labels = containerUtils.mkTraefikLabels {
-          name = "authentik";
-          port = 9000;
-          extraLabels = {
-            "traefik.http.middlewares.authentik-headers.headers.customrequestheaders.X-Forwarded-Proto" = "https";
-            "traefik.http.routers.authentik.middlewares" = "authentik-headers";
+        authentik-server = {
+          image = "ghcr.io/goauthentik/server:2026.8.3";
+          cmd = ["server"];
+          dependsOn = ["authentik-postgres"];
+          networks = ["container:authentik-postgres"];
+          environment = {
+            AUTHENTIK_POSTGRESQL__HOST = "localhost";
+            AUTHENTIK_POSTGRESQL__NAME = "authentik";
+            AUTHENTIK_POSTGRESQL__USER = "authentik";
+            AUTHENTIK_WEB__BASE_URL = "https://authentik.${cfg.domain}";
+            TZ = config.time.timeZone;
+          };
+          environmentFiles = [secretsFile];
+          extraOptions = [
+            "--shm-size=512mb"
+          ];
+          volumes = [
+            "${authentikPath}/data:/data"
+            "${authentikPath}/custom-templates:/templates"
+          ];
+          labels = containerUtils.mkTraefikLabels {
+            name = "authentik";
+            port = 9000;
+            extraLabels = {
+              "traefik.enable" = "true";
+              "traefik.http.routers.${name}.rule" = "Host(`${name}.${cfg.domain}`)";
+              "traefik.http.routers.${name}.entrypoints" = "websecure";
+              "traefik.http.routers.${name}.tls" = "true";
+
+              "traefik.http.services.${name}.loadbalancer.server.port" = toString port;
+              "traefik.http.middlewares.authentik-forward-auth.forwardauth.address" = "http://authentik-server:9000/outpost.goauthentik.io/auth/traefik";
+              "traefik.http.middlewares.authentik-forward-auth.forwardauth.trustForwardHeader" = "true";
+              "traefik.http.middlewares.authentik-forward-auth.forwardauth.authResponseHeaders" = "X-authentik-username,X-authentik-groups,X-authentik-email,X-authentik-name,X-authentik-uid,X-authentik-jwt,X-authentik-meta-jwks,X-authentik-meta-outpost,X-authentik-meta-provider,X-authentik-meta-app,X-authentik-meta-version";
+            };
           };
         };
-      };
 
-      authentik-worker = {
-        image = "ghcr.io/goauthentik/server:2026.8.3";
-        cmd = ["worker"];
-        dependsOn = ["authentik-postgres"];
-        networks = ["container:authentik-postgres"];
-        environment = {
-          AUTHENTIK_POSTGRESQL__HOST = "localhost";
-          AUTHENTIK_POSTGRESQL__NAME = "authentik";
-          AUTHENTIK_POSTGRESQL__USER = "authentik";
-          AUTHENTIK_WEB__BASE_URL = "https://authentik.${cfg.domain}";
-          AUTHENTIK_LISTEN__HTTP = "0.0.0.0:9100";
-          AUTHENTIK_LISTEN__METRICS = "0.0.0.0:9301";
-          TZ = config.time.timeZone;
+        authentik-worker = {
+          image = "ghcr.io/goauthentik/server:2026.8.3";
+          cmd = ["worker"];
+          dependsOn = ["authentik-postgres"];
+          networks = ["container:authentik-postgres"];
+          environment = {
+            AUTHENTIK_POSTGRESQL__HOST = "localhost";
+            AUTHENTIK_POSTGRESQL__NAME = "authentik";
+            AUTHENTIK_POSTGRESQL__USER = "authentik";
+            AUTHENTIK_WEB__BASE_URL = "https://authentik.${cfg.domain}";
+            AUTHENTIK_LISTEN__HTTP = "0.0.0.0:9100";
+            AUTHENTIK_LISTEN__METRICS = "0.0.0.0:9301";
+            TZ = config.time.timeZone;
+          };
+          environmentFiles = [secretsFile];
+          extraOptions = [
+            "--shm-size=512mb"
+          ];
+          user = "root";
+          volumes = [
+            "/var/run/docker.sock:/var/run/docker.sock"
+            "${authentikPath}/data:/data"
+            "${authentikPath}/certs:/certs"
+            "${authentikPath}/custom-templates:/templates"
+          ];
         };
-        environmentFiles = [secretsFile];
-        extraOptions = [
-          "--shm-size=512mb"
-        ];
-        user = "root";
-        volumes = [
-          "/var/run/docker.sock:/var/run/docker.sock"
-          "${authentikPath}/data:/data"
-          "${authentikPath}/certs:/certs"
-          "${authentikPath}/custom-templates:/templates"
-        ];
       };
     };
   };
-};
 }
--- nixos/modules/containers/containers.nix
+++ nixos/modules/containers/containers.nix
@@ -1,72 +1,90 @@
 {...}: {
-  nixos.containers =
-    {
-      lib,
-      pkgs,
-      config,
-      ...
-    }:
-    let
-      cfg = config.modules.nix.containers;
-    in {
-      options.modules.nix.containers = with lib; {
-    domain = mkOption {
-      type = types.str;
-      description = "Base domain used for Traefik-routed services.";
-    };
-
-    mainUser = mkOption {
-      type = types.str;
-      description = "Primary user that owns container data directories.";
-    };
-
-    dataPath = mkOption {
-      type = types.path;
-      default = "/home/${config.modules.nix.containers.mainUser}/appdata";
-      description = "Base path for container persistent data.";
-    };
-  };
-
-  config._module.args.containerUtils = {
-    mkTraefikLabels = {
-      name,
-      port,
-      extraLabels ? {},
-    }:
-      lib.mkMerge [
-        {
-          "traefik.enable" = "true";
-          "traefik.http.routers.${name}.rule" = "Host(`${name}.${cfg.domain}`)";
-          "traefik.http.routers.${name}.entrypoints" = "websecure";
-          "traefik.http.routers.${name}.tls" = "true";
-
-          "traefik.http.services.${name}.loadbalancer.server.port" = toString port;
-        }
-        extraLabels
-      ];
-    mkDataPath = name: "${cfg.dataPath}/${name}";
-    storagePaths = rec {
-      # base = "/storage";
-      base = "/mnt/isa/media";
-      torrents = rec {
-        dir = "${base}/torrents";
-        subdirs = {
-          books = "${dir}/books";
-          movies = "${dir}/movies";
-          music = "${dir}/music";
-          tv = "${dir}/tv";
-        };
+  nixos.containers = {
+    lib,
+    pkgs,
+    config,
+    ...
+  }: let
+    cfg = config.modules.nix.containers;
+  in {
+    options.modules.nix.containers = with lib; {
+      domain = mkOption {
+        type = types.str;
+        description = "Base domain used for Traefik-routed services.";
       };
-      media = rec {
-        dir = "${base}/media";
-        subdirs = {
-          books = "${dir}/books";
-          movies = "${dir}/movies";
-          music = "${dir}/music";
-          tv = "${dir}/tv";
-        };
+
+      mainUser = mkOption {
+        type = types.str;
+        description = "Primary user that owns container data directories.";
+      };
+
+      dataPath = mkOption {
+        type = types.path;
+        default = "/home/${config.modules.nix.containers.mainUser}/appdata";
+        description = "Base path for container persistent data.";
       };
     };
+
+    config._module.args.containerUtils = {
+      mkTraefikLabels = {
+        name,
+        port,
+        extraLabels ? {},
+      }:
+        lib.mkMerge [
+          {
+            "traefik.enable" = "true";
+            "traefik.http.routers.${name}.rule" = "Host(`${name}.${cfg.domain}`)";
+            "traefik.http.routers.${name}.entrypoints" = "websecure";
+            "traefik.http.routers.${name}.tls" = "true";
+
+            "traefik.http.services.${name}.loadbalancer.server.port" = toString port;
+          }
+          extraLabels
+        ];
+      mkTraefikLabelsWithAuth = {
+        name,
+        port,
+        extraLabels ? {},
+      }:
+        lib.mkMerge [
+          {
+            "traefik.enable" = "true";
+            "traefik.http.routers.${name}.rule" = "Host(`${name}.${cfg.domain}`) && PathPrefix(`/outpost.goauthentik.io/`)";
+            "traefik.http.routers.${name}.entrypoints" = "websecure";
+            "traefik.http.routers.${name}.tls" = "true";
+
+            "traefik.http.routers.${name}-outpost.priority" = "15";
+            "traefik.http.routers.${name}-outpost.service" = "authentik@docker";
+            "traefik.http.routers.${name}.middlewares" = "authentik-forward-auth@docker";
+
+            "traefik.http.services.${name}.loadbalancer.server.port" = toString port;
+          }
+          extraLabels
+        ];
+      mkDataPath = name: "${cfg.dataPath}/${name}";
+      storagePaths = rec {
+        # base = "/storage";
+        base = "/mnt/isa/media";
+        torrents = rec {
+          dir = "${base}/torrents";
+          subdirs = {
+            books = "${dir}/books";
+            movies = "${dir}/movies";
+            music = "${dir}/music";
+            tv = "${dir}/tv";
+          };
+        };
+        media = rec {
+          dir = "${base}/media";
+          subdirs = {
+            books = "${dir}/books";
+            movies = "${dir}/movies";
+            music = "${dir}/music";
+            tv = "${dir}/tv";
+          };
+        };
+      };
+    };
   };
-};
 }
