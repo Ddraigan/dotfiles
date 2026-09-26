d8cc3a6956a6dfee736d55d42c51631f18bb4ea5
Ddraigan <lkjjones1999@gmail.com>
2026-09-26 16:19:45 +0100

    added kuma to auth

--- nixos/modules/containers/uptime-kuma.nix
+++ nixos/modules/containers/uptime-kuma.nix
@@ -1,40 +1,38 @@
 {...}: {
-  nixos.uptime-kuma =
-    {
-      lib,
-      pkgs,
-      config,
-      containerUtils,
-      ...
-    }:
-    let
-      cfg = config.modules.nix.containers;
-      kumaPath = "${cfg.dataPath}/uptime-kuma";
-    in {
-      config = {
-    systemd.tmpfiles.rules = [
-      "d ${kumaPath} 0755 ${cfg.mainUser} users -"
-    ];
-    virtualisation.oci-containers = {
-      containers = {
-        uptime-kuma = {
-          image = "louislam/uptime-kuma:2.5.0";
-          volumes = [
-            "${kumaPath}:/app/data"
-            "/var/run/docker.sock:/var/run/docker.sock"
-          ];
-          labels = containerUtils.mkTraefikLabels {
-            name = "uptime-kuma";
-            port = 3001;
-            extraLabels = {
-              # WebSocket support
-              "traefik.http.middlewares.uptime-kuma-headers.headers.customrequestheaders.X-Forwarded-Proto" = "https";
-              "traefik.http.routers.uptime-kuma.middlewares" = "uptime-kuma-headers";
+  nixos.uptime-kuma = {
+    lib,
+    pkgs,
+    config,
+    containerUtils,
+    ...
+  }: let
+    cfg = config.modules.nix.containers;
+    kumaPath = "${cfg.dataPath}/uptime-kuma";
+  in {
+    config = {
+      systemd.tmpfiles.rules = [
+        "d ${kumaPath} 0755 ${cfg.mainUser} users -"
+      ];
+      virtualisation.oci-containers = {
+        containers = {
+          uptime-kuma = {
+            image = "louislam/uptime-kuma:2.5.0";
+            volumes = [
+              "${kumaPath}:/app/data"
+              "/var/run/docker.sock:/var/run/docker.sock"
+            ];
+            labels = containerUtils.mkTraefikLabelsWithAuth {
+              name = "uptime-kuma";
+              port = 3001;
+              extraMiddlewares = ["uptime-kuma-headers@docker"];
+              extraLabels = {
+                # WebSocket support
+                "traefik.http.middlewares.uptime-kuma-headers.headers.customrequestheaders.X-Forwarded-Proto" = "https";
+              };
             };
           };
         };
       };
     };
   };
-};
 }
